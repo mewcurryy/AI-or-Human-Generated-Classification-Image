a7a5ee940d0d273b0084a59ba0505f62590f4277
Davin P <110343007+mewcurryy@users.noreply.github.com>
2025-09-27 13:02:34 +0700

    editing app.py skibidis

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def main():
             
         col1, col2 = st.columns(2)
         with col1:
-            st.image(image, caption="Gambar yang Diunggah", use_column_width=True)
+            st.image(image, caption="Gambar yang Diunggah", use_container_width=True)
             
         with st.spinner('Menganalisis gambar...'):
             processed_image = preprocess_image(image)
